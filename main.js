304b5ce8d5bdac2664a3fbe690830cae674eea44
hermy24 <132006969+hermy24@users.noreply.github.com>
2023-12-16 11:15:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,131 @@
+function clear(){
+   document.getElementById("output").innerHTML="";
+}
+function result(){
+   if(document.getElementById("input").value==""){
+      document.getElementById("output").innerHTML="";
+     }
+   if(document.getElementById("input").value=="a"){
+    document.getElementById("output").innerHTML=". -";
+   }
+   if(document.getElementById("input").value=="b"){
+    document.getElementById("output").innerHTML="-...";
+   }
+   if(document.getElementById("input").value=="c"){
+    document.getElementById("output").innerHTML="-.-.";
+   }
+   if(document.getElementById("input").value=="d"){
+    document.getElementById("output").innerHTML="-..";
+   }
+   if(document.getElementById("input").value=="e"){
+    document.getElementById("output").innerHTML=".";
+   }
+   if(document.getElementById("input").value=="f"){
+    document.getElementById("output").innerHTML="..-.";
+   }
+   if(document.getElementById("input").value=="g"){
+    document.getElementById("output").innerHTML="--.";
+   }
+   if(document.getElementById("input").value=="h"){
+    document.getElementById("output").innerHTML="....";
+   }
+   if(document.getElementById("input").value=="i"){
+    document.getElementById("output").innerHTML="..";
+   }
+   if(document.getElementById("input").value=="j"){
+    document.getElementById("output").innerHTML=".---";
+   }
+   if(document.getElementById("input").value=="k"){
+    document.getElementById("output").innerHTML="-.-";
+   }
+   if(document.getElementById("input").value=="l"){
+    document.getElementById("output").innerHTML=".-..";
+   }
+   if(document.getElementById("input").value=="m"){
+    document.getElementById("output").innerHTML="--";
+   }
+   if(document.getElementById("input").value=="n"){
+    document.getElementById("output").innerHTML="-.";
+   }
+   if(document.getElementById("input").value=="o"){
+    document.getElementById("output").innerHTML="---";
+   }
+   if(document.getElementById("input").value=="p"){
+    document.getElementById("output").innerHTML=".--.";
+   }
+   if(document.getElementById("input").value=="q"){
+    document.getElementById("output").innerHTML="--.-";
+   }
+   if(document.getElementById("input").value=="r"){
+      document.getElementById("output").innerHTML=".-.";
+     }
+     if(document.getElementById("input").value=="s"){
+      document.getElementById("output").innerHTML="...";
+     }
+     if(document.getElementById("input").value=="t"){
+      document.getElementById("output").innerHTML="-";
+     }
+     if(document.getElementById("input").value=="u"){
+      document.getElementById("output").innerHTML="..-";
+     }
+     if(document.getElementById("input").value=="v"){
+      document.getElementById("output").innerHTML="...-";
+     }
+     if(document.getElementById("input").value=="w"){
+      document.getElementById("output").innerHTML=".--";
+     }
+     if(document.getElementById("input").value=="x"){
+      document.getElementById("output").innerHTML="-..-";
+     }
+     if(document.getElementById("input").value=="y"){
+      document.getElementById("output").innerHTML="-.--";
+     }
+     if(document.getElementById("input").value=="z"){
+      document.getElementById("output").innerHTML="--..";
+     }
+     if(document.getElementById("input").value=="1"){
+      document.getElementById("output").innerHTML=".----";
+     }
+     if(document.getElementById("input").value=="2"){
+      document.getElementById("output").innerHTML="..---";
+     }
+     if(document.getElementById("input").value=="3"){
+      document.getElementById("output").innerHTML="...--";
+     }
+     if(document.getElementById("input").value=="4"){
+      document.getElementById("output").innerHTML="....-";
+     }
+     if(document.getElementById("input").value=="5"){
+      document.getElementById("output").innerHTML=".....";
+     }
+     if(document.getElementById("input").value=="6"){
+      document.getElementById("output").innerHTML="-....";
+     }
+     if(document.getElementById("input").value=="7"){
+      document.getElementById("output").innerHTML="--...";
+     }
+     if(document.getElementById("input").value=="8"){
+      document.getElementById("output").innerHTML="---..";
+     }
+     if(document.getElementById("input").value=="9"){
+      document.getElementById("output").innerHTML="----.";
+     }
+     if(document.getElementById("input").value=="0"){
+      document.getElementById("output").innerHTML="-----";
+     }
+     if(document.getElementById("input").value=="10"){
+      document.getElementById("output").innerHTML=".---- -----";
+     }}
+   
+
+
+  
+
+
+  
+
+
+
+
+var array1=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0"];
+var array_morse=[,"","","","","","","","","","",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..",".----","..---","...--","....-","",".....","-....","--...","---..","----.","-----"];
